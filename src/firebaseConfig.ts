1adf7256f6080a6d2207c6b86782e66ff823e4be
Vladimir Markovic <markovic.gi11.2021@uns.ac.rs>
2025-12-03 22:14:27 +0100

    Initial commit

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -9,13 +9,13 @@ import "firebase/database";
 // 4. Copy the 'firebaseConfig' object shown there and paste the values below.
 
 const firebaseConfig = {
-  "apiKey": "AIzaSyDyxtpA_FI8_gqUz2dHv1ceBFe7W1zHWpE",
-  "authDomain": "arched-lens-451511-d3.firebaseapp.com",
-  "projectId": "arched-lens-451511-d3",
-  "storageBucket": "arched-lens-451511-d3.firebasestorage.app",
-  "messagingSenderId": "314383384865",
-  "appId": "1:314383384865:web:c15993a40411a240273e60",
-  "measurementId": "G-Z1GJ2D8MY0"
+  apiKey: "AIzaSyDyxtpA_FI8_gqUz2dHv1ceBFe7W1zHWpE",
+  authDomain: "arched-lens-451511-d3.firebaseapp.com",
+  projectId: "arched-lens-451511-d3",
+  storageBucket: "arched-lens-451511-d3.firebasestorage.app",
+  messagingSenderId: "314383384865",
+  appId: "1:314383384865:web:c15993a40411a240273e60",
+  measurementId: "G-Z1GJ2D8MY0"
 };
 
 // Initialize Firebase (Singleton check)
